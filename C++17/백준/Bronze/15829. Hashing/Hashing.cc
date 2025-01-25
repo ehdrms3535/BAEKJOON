16870ee34d0ef68195e48c1a1f939f942bbd6a57
ehdrms3535 <88962038+ehdrms3535@users.noreply.github.com>
2025-01-25 15:14:45 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/15829. Hashing/Hashing.cc
+++ C++17/백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,13 +4,25 @@
 using namespace std;
 
 int main() {
-	int T,sum=0,M=1234567891;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long T,sum=0,M=1234567891;
 	string a;
 
 	cin >> T >> a;
 	for (int i = 0; i < T; i++) {
-		sum += ((int)a[i]-96) * pow(31,i);
-	}
+		long long ex = i;
+		long long result=1,base = 31;
+		while (ex > 0) {
+			if (ex % 2 == 1) {
+				result = (base * result) % M;
+			}
+			base = base * base % M;
+			ex /= 2;
+		}
+		sum += ((a[i]-96)*result)%M;
+		}
 	cout << sum%M;
 
 	//a = 97,z=122;
