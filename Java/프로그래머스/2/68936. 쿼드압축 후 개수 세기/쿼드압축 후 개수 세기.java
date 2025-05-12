4172a1b2fce77a023861072d2d8cdce8d5992cb6
ehdrms3535 <88962038+ehdrms3535@users.noreply.github.com>
2025-05-12 20:31:06 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 127.52 ms, Memory: 109 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,50 @@
+class Solution{
+    boolean Check(int array[][]){
+        int row = array.length;
+        int col = row;
+        int tmp = array[0][0];
+
+        for(int i=0; i<row;i++){
+            for(int j=0;j<col;j++){
+                if(array[i][j]!=tmp) return false;
+            }
+        }
+        return true;
+    }
+
+    int[][] Sub(int[][] arr, int startRow, int startCol, int endRow, int endCol) {
+        int[][] result = new int[endRow - startRow][endCol - startCol];
+        for (int i = startRow; i < endRow; i++) {
+            for (int j = startCol; j < endCol; j++) {
+                result[i - startRow][j - startCol] = arr[i][j];
+            }
+        }
+        return result;
+    }
+
+    void quad(int array[][],int[] answer){
+        int row=array.length;
+        int col = row;
+        if( row == 1 || Check(array)) {
+            answer[array[0][0]]++;
+            return;
+        }
+
+        else{
+            quad(Sub(array,0,0,row/2,col/2),answer);
+            quad(Sub(array,row/2,0,row,col/2),answer);
+            quad(Sub(array,0,col/2,row/2,col),answer);
+            quad(Sub(array,row/2,col/2,row,col),answer);
+        }
+    }
+
+    int[] solution(int[][] array){
+        int row = array.length;
+        int col = row;
+
+        int[] answer = {0,0};
+
+        quad(array,answer);
+        return answer;
+    }
+}
